cde51e9a71c7879671789d559cb235d6788de3e5
Pavel Picka <ppicka@redhat.com>
2019-09-04 14:00:49 +0200

    Remove comment from script
    as it is same as README.md file
    
    Signed-off-by: Pavel Picka <ppicka@redhat.com>

--- fotp_migrate.py
+++ fotp_migrate.py
@@ -1,28 +1,5 @@
 #!/usr/bin/env python3
-"""
-Get FreeOTP tokens.
-
-Steps to get tokens.xml
-1. Android -> Settings -> System -> About -> Buil number (tap,tap,tap)
-  to get android developer mode
-2. In "Developer options" allow "USB debugging"
-3. Connect phone to PC
-
-ADB backup (case for FreeOTP)
-1. install 'adb'
-2. adb backup -f freeotp-backup.ab -apk org.fedorahosted.freeotp
-3. asked for password so entry some <password>
-
-In case encrypted phone
-1. download https://github.com/nelenkov/android-backup-extractor/releases
-2. java -jar abe-all.jar unpack freeotp-backup.ab freeotp.tar <password>
-3. tar xvf freeotp.tar
-4. tokens are in apps/org.fedorahosted.freeotp/sp/tokens.xml
-5. run this script in same folder as 'tokens.xml'
-
-Do import after export in short time frame as OTP counter can get raised and
-your generated tokens won't work.
-"""
+"""Migrate FreeOTP tokens.xml to importable JSON for FreeOTP+."""
 
 import argparse
 import json
